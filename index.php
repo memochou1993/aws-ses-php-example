0bced8a323f565ea69c2dbd14e2507504b046342
William Chou <williamchou@kklab.com>
2024-01-27 15:00:37 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+require './vendor/autoload.php';
+
+use Aws\Sdk;
+use Aws\Exception\AwsException;
+
+$sharedConfig = [
+    'region' => 'ap-northeast-1',
+];
+
+$sdk = new Sdk($sharedConfig);
+
+$ses = $sdk->createSes();
+
+$sender_email = 'user@example.com';
+
+$recipient_emails = ['memochou1993@hotmail.com'];
+
+$subject = 'Amazon SES test (AWS SDK for PHP)';
+$plaintext_body = 'This email was sent with Amazon SES using the AWS SDK for PHP.' ;
+$html_body =  '<h1>AWS Amazon Simple Email Service Test Email</h1>'.
+              '<p>This email was sent with <a href="https://aws.amazon.com/ses/">'.
+              'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">'.
+              'AWS SDK for PHP</a>.</p>';
+$char_set = 'UTF-8';
+
+try {
+    $result = $ses->sendEmail([
+        'Destination' => [
+            'ToAddresses' => $recipient_emails,
+        ],
+        'ReplyToAddresses' => [$sender_email],
+        'Source' => $sender_email,
+        'Message' => [
+          'Body' => [
+              'Html' => [
+                  'Charset' => $char_set,
+                  'Data' => $html_body,
+              ],
+              'Text' => [
+                  'Charset' => $char_set,
+                  'Data' => $plaintext_body,
+              ],
+          ],
+          'Subject' => [
+              'Charset' => $char_set,
+              'Data' => $subject,
+          ],
+        ],
+    ]);
+    $messageId = $result['MessageId'];
+    echo("Email sent! Message ID: $messageId"."\n");
+} catch (AwsException $e) {
+    // output error message if fails
+    echo $e->getMessage();
+    echo("The email was not sent. Error message: ".$e->getAwsErrorMessage()."\n");
+    echo "\n";
+}
